cf4789e2cf1ebc682ca286a92178d36da204db25
yash <yashchaurasia630@gmail.com>
2025-11-17 16:09:30 +0530

    multiple cubes with rotation

--- 3D/main.cpp
+++ 3D/main.cpp
@@ -84,6 +84,18 @@ int main()
       -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
       -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
+  glm::vec3 cubePositions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),
+      glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f),
+      glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),
+      glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),
+      glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),
+      glm::vec3(-1.3f, 1.0f, -1.5f)};
+
   // GL DRAW ELEMENTS DOESNT WORK AS YOU NEED SPECIFIC UV COORDINATES FOR EACH VERTEX ACCORDING TO EACH FACE
   // float attr[] = {
   //     // FRONT FACE
@@ -99,24 +111,24 @@ int main()
   // };
 
   // unsigned int indices[] = {
-      //     // FRONT FACE
-      //     0, 1, 2,
-      //     2, 3, 0,
-      //     // BACK FACE
-      //     4, 5, 6,
-      //     6, 7, 4,
-      //     // TOP FACE
-      //     3, 2, 6,
-      //     6, 7, 3,
-      //     // BOTTOM FACE
-      //     0, 1, 5,
-      //     5, 4, 0,
-      //     // LEFT FACE
-      //     0, 3, 7,
-      //     7, 4, 0,
-      //     // RIGHT FACE
-      //     1, 2, 6,
-      // 6, 5, 1
+  //     // FRONT FACE
+  //     0, 1, 2,
+  //     2, 3, 0,
+  //     // BACK FACE
+  //     4, 5, 6,
+  //     6, 7, 4,
+  //     // TOP FACE
+  //     3, 2, 6,
+  //     6, 7, 3,
+  //     // BOTTOM FACE
+  //     0, 1, 5,
+  //     5, 4, 0,
+  //     // LEFT FACE
+  //     0, 3, 7,
+  //     7, 4, 0,
+  //     // RIGHT FACE
+  //     1, 2, 6,
+  // 6, 5, 1
   // };
 
   unsigned int VBO, VAO, EBO;
@@ -174,29 +186,36 @@ int main()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    glBindTexture(GL_TEXTURE_2D, texture);
     ourShader.use();
 
     // matrix stuff
-    glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection;
 
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-    unsigned int glModelLoc = glGetUniformLocation(ourShader.ID, "model");
     unsigned int glViewLoc = glGetUniformLocation(ourShader.ID, "view");
     unsigned int glProjectionLoc = glGetUniformLocation(ourShader.ID, "projection");
 
-    glUniformMatrix4fv(glModelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(glViewLoc, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(glProjectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
     // opengl stuff again
     glBindVertexArray(VAO);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    for (unsigned int i = 0; i < 10; i++)
+    {
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      float angle = glfwGetTime() * 15 * (i + 1);
+      model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.5f, 0.3f));
+      unsigned int glModelLoc = glGetUniformLocation(ourShader.ID, "model");
+      glUniformMatrix4fv(glModelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
     // glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
