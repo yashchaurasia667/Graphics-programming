35e150f1f4fc04641ebb43de62f1a66eff27619a
yash <yashchaurasia630@gmail.com>
2026-03-06 14:21:05 +0530

    linux setup

--- include/utils.h
+++ include/utils.h
@@ -3,9 +3,15 @@
 #include <glad/glad.h>
 #include <iostream>
 
-#define ASSERT(x) \
-  if (!x)         \
-    __debugbreak();
+#ifdef _MSC_VER
+  #define ASSERT(x) if (!(x)) __debugbreak()
+#elif defined(__GNUC__) || defined(__clang__)
+  #define ASSERT(x) if (!(x)) __builtin_trap()
+#else
+  #include <signal.h>
+  #define ASSERT(x) if (!(x)) raise(SIGTRAP)
+#endif
+
 #define GLcall(x) \
   GLclearError(); \
   x;              \
